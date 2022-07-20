9d5ef88413203d2daf80401194974b01dc30f7b3
Danyil <danianyk@gmail.com>
2022-07-20 16:25:55 +0300

    Initial Commit

---
+++ e_store/store/models.py
@@ -0,0 +1,72 @@
+from itertools import product
+from statistics import mode
+from tkinter import CASCADE
+from unicodedata import name
+from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ImageField
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    name=models.CharField(max_length=200,null=True)
+    email = models.CharField(max_length=200,null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return str(self.id)
+    
+    @property
+    def get_cart_total(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.get_total for item in order_items])
+        return total
+    
+    @property
+    def get_cart_items(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.quantity for item in order_items])
+        return total
+
+class Product(models.Model):
+    name = models.CharField(max_length=200,null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True)
+    image = models.ImageField(null=True, blank=True, default="product_placeholder.png")
+
+    def __str__(self):
+        return self.name
+    
+    
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True,blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
+
+class  ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    region = models.CharField(max_length=200)
+    zipcode = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
+    
